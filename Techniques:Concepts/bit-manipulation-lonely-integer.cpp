769a663523af4112006bb7504fae441c8a64f68c
Jeet Mehta <jeet25.m@gmail.com>
2017-09-27 20:55:55 -0400

    Bit Manipulation: Lonely Integer

---
+++ Techniques:Concepts/bit-manipulation-lonely-integer.cpp
@@ -0,0 +1,44 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int lonely_integer(vector < int > a) {
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (count(a.begin(), a.end(), a[i]) == 1)
+            return a[i];
+    }
+    return 0;    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+       cin >> a[a_i];
+    }
+    cout << lonely_integer(a) << endl;
+    return 0;
+}
